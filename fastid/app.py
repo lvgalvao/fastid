54a3c03ab3dad090014500cea12e7cd8d87bf89a
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-30 05:05:36 -0300

    feat: adicionar templates home.html

--- fastid/app.py
+++ fastid/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
 
 app = FastAPI()
+app.mount(
+    '/static', 
+    StaticFiles(directory='static'),
+    name='static'
+)
+
+templates = Jinja2Templates(directory='templates')
 
 @app.get("/")
-async def read_root():
-    return {"message": "Hello World!"}
+async def read_root(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request=request, name='home.html')
