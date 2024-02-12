bd823a2768f3e4809242afc08624bf55424ef123
Luciano Filho <lvgalvaofilho@gmail.com>
2024-02-12 08:19:01 -0300

    feat: adicionar login logout

--- fastid/app.py
+++ fastid/app.py
@@ -1,12 +1,17 @@
+from json import dumps
+
 from authlib.integrations.starlette_client import OAuth
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.requests import Request
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from starlette.middleware.sessions import SessionMiddleware
+
 class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(
@@ -16,6 +21,8 @@ class Settings(BaseSettings):
     DOMAIN: str
     CLIENT_ID: str
     CLIENT_SECRET: str
+    SECRET_KEY: str
+    AUDIENCE: str = ''
 
 settings = Settings()
 
@@ -31,18 +38,68 @@ oauth.register(
 )
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.mount(
     '/static', 
     StaticFiles(directory='static'),
     name='static'
 )
 
+def to_pretty_json(object: dict) -> str:
+    return dumps(object, 
+                 default= lambda x: dict(x),
+                 indent=4
+                 )
+
 templates = Jinja2Templates(directory='templates')
+templates.env.filters['to_pretty_json'] = to_pretty_json
+
 
 @app.get("/")
 async def read_root(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request=request, name='home.html')
 
-@app.get("/login")
-async def login(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(request=request, name='profile.html')
+@app.get('/login')
+async def login(request: Request):
+    if (
+        'id_token' not in request.session
+    ):  # it could be userinfo instead of id_token
+        return await oauth.auth0.authorize_redirect(
+            request,
+            redirect_uri=request.url_for('callback'),
+            audience=settings.AUDIENCE,
+        )
+    return RedirectResponse('/')
+
+@app.get('/logout')
+async def logout(request: Request):
+    response = RedirectResponse(
+        url='https://'
+        + settings.DOMAIN
+        + '/v2/logout?'
+        + urlencode(
+            {
+                'returnTo': request.url_for('home'),
+                'client_id': settings.CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        )
+    )
+    request.session.clear()
+    return response
+
+@app.get('/callback')
+async def callback(request: Request):
+    token = await oauth.auth0.authorize_access_token(request)
+    request.session['access_token'] = token['access_token']
+    request.session['id_token'] = token['id_token']
+    request.session['userinfo'] = token['userinfo']
+    return RedirectResponse('/')
+
+@app.get('/profile')
+def profile(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='profile.html',
+        context={'request': request, 'userinfo': request.session['userinfo']},
+    )
